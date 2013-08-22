a3d14e569a8d5e234f8aab0ec10f08139bb338f8
vagrant <vagrant@precise64.(none)>
2013-08-22 19:49:12 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookbuzz::Application.config.secret_key_base = '494f8bf4efcdc2bbe2f2cf5a06957ef4531f2b7bd0d31ce6754e919193e25c2ecd0259769d0abbf90220ce41b2efdcdb6aa84d2546c5d23cef91c6873dab503e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bookbuzz::Application.config.session_store :cookie_store, key: '_bookbuzz_session'
