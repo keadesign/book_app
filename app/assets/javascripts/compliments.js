eeb7ae0649c6109891ef647fcc55a20905f7dbfd
vagrant <vagrant@precise64.(none)>
2013-08-26 17:33:54 +0000

    Add This buttons

--- app/assets/javascripts/compliments.js
+++ app/assets/javascripts/compliments.js
@@ -1,40 +1,25 @@
 var compliments = [
   {
-    "color":"#0080FF",
-    "phrase":"Your prom date still thinks about you all the time.",
-    "link":"http://society6.com/emergencycompliment/Your-Prom-Date_Print"
-  },
-  {
-    "color":"#01DF3A",
-    "phrase":"All your friends worry they aren\u2019t as funny as you.",
-    "link":"http://society6.com/emergencycompliment/As-Funny-As-You-BSA_Print"
-  },
-  {
-    "color":"#FFBF00",
-    "phrase":"Your boss loved that thing you did at work today.",
-    "link":"http://society6.com/emergencycompliment/Your-Boss-Loved-That_Print"
-  },
-  {
     // Start Something That Matters
     "color":"#FF00BF",
     "phrase":"Do you juice? Do you want to save the world with your hippy business idea and recycled bottlecaps?",
     "link":"http://www.amazon.com/gp/product/0812981448/ref=as_li_tf_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=0812981448&linkCode=as2&tag=keadesign"
   },
-  // Manchu Picchu
   {
-    "color":"#000000",
+    // Manchu Picchu
+    "color":"#0080FF",
     "phrase":"Are you over thirty? Is Indiana Jones your hero, but your recliner your homebase?",
     "link":"http://www.amazon.com/gp/product/0452297982/ref=as_li_tf_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=0452297982&linkCode=as2&tag=keadesign"
   },
   {
     // Dearie
-    "color":"#000000",
+    "color":"#01DF3A",
     "phrase":"Dearie",
     "link":"http://www.amazon.com/gp/product/0307473414/ref=as_li_tf_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=0307473414&linkCode=as2&tag=keadesign"
   },
   {
     // Cuckoo's Calling
-    "color":"#000000",
+    "color":"#FFBF00",
     "phrase":"Cuckoo's Calling",
     "link":"http://www.amazon.com/gp/product/0316330167/ref=as_li_tf_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=0316330167&linkCode=as2&tag=keadesign"
   },
